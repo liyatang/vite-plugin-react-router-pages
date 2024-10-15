10991cf4946d624c7b0164eaa5591cd07e287738
liyatang <liyatang@qq.com>
2024-10-15 22:25:10 +0800

    fix: path.join change to path.posix.join。修复 windows 的问题。

--- src/generate.ts
+++ src/generate.ts
@@ -78,7 +78,7 @@ interface Config {
 function getFiles(config: Config) {
   // 获取匹配到的文件
   // [ 'src/pages/demo/index.page.tsx', '/src/pages/demo/layout.tsx', 'src/pages/demo/info/index.page.tsx']
-  let files = globSync(path.join(config.pagesPath, '**/{index.page,layout}.{jsx,tsx}'));
+  let files = globSync(path.posix.join(config.pagesPath, '**/{index.page,layout}.{jsx,tsx}'));
   // glob 返回 src/pages/xxxx，所以补下 ./
   return files.map((file) => `./${file}`);
 }
